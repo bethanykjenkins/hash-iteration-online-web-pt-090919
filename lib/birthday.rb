291830bd31c48d40328453fd8de306acc7b6046b
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-10-02 14:49:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,8 +14,10 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   if age <= 12
+     birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   else
     puts "You are too old for this."
   end
+end
 end
